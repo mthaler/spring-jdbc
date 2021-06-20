af92bc97397b953fae6cd07355a5b14dbbce710d
Michael Thaler <michael.thaler@dlr.de>
2021-06-20 08:39:21 +0200

    Added Spring initializer code.

---
+++ src/main/kotlin/com/mthaler/springjdbc/SpringjdbcApplication.kt
@@ -0,0 +1,11 @@
+package com.mthaler.springjdbc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringjdbcApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringjdbcApplication>(*args)
+}
---
+++ src/test/kotlin/com/mthaler/springjdbc/SpringjdbcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mthaler.springjdbc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringjdbcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
